590dc3da9560a35af2fbea821f2ea92fbee26104
Riley O'Shea <RileyOShea@fairisaac.com>
2018-09-11 12:05:16 -0700

    Start additional exercises

--- src/week1/max_pairwise_prod.rs
+++ src/week1/max_pairwise_prod.rs
@@ -50,16 +50,50 @@ pub fn run_naive_fast(mut b: Vec<i64>) -> i64 {
     b[n - 1] * b[n]
 }
 pub fn run_naive_faster(b: Vec<i64>) -> i64 {
-    let mut ix = 0;
+    let mut ix  = 0;
     let mut arr_l: Vec<i64> = Vec::new();
     let mut arr_s: Vec<i64> = Vec::new();
+
+    let n = b.len() - 1;
+    for i in (0..n+1).filter(|x| x % 2 == 0) {
+        if i + 1 >= b.len() {
+            arr_l.push(b[i]);
+            continue
+        }
+
+        if b[i] > b[i+1] {
+            arr_l.push(b[i]);
+            arr_s.push(b[i+1]);
+        } else {
+            arr_l.push(b[i+1]);
+            arr_s.push(b[i]);
+        }
+    }
+
+    for i in 0..arr_l.len() {
+        if arr_l[i] > arr_l[ix] {
+            ix = i;
+        }
+    }
+    //FIXME:
+    let mut ix_s  = 0;
+    for i in 0..arr_l.len() {
+        if i != ix && arr_l[i] > arr_l[ix_s] {
+            ix_s = i;
+        }
+    }
+    let mut s = 0;
+    if arr_l[ix_s] < arr_s[ix] || ix_s == ix {
+        s = arr_s[ix];
+    }
+
 
 
     println!("{:?}", b);
     println!("{:?}", arr_l);
     println!("{:?}", arr_s);
-    println!("{} {}", b[ix], b[ix_s]);
+    println!("{} {}", arr_l[ix], arr_l[ix_s]);
     println!("{} {}", ix, ix_s);
-    b[ix] * b[ix_s]
+    (arr_l[ix] * arr_l[ix_s]) as i64
 }
 
